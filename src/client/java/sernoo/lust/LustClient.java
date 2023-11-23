299187a314b361d7d3a4adceb8f1999b717aba1e
sernoo <151878412+sernoo@users.noreply.github.com>
2023-11-23 15:40:16 -0500

    Initial commit

---
+++ src/client/java/sernoo/lust/LustClient.java
@@ -0,0 +1,10 @@
+package sernoo.lust;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LustClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
