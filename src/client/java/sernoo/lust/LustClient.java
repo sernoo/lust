cf65aa23b7100c190266d961fb8a5b18e2dc5a00
sernoo <151878412+sernoo@users.noreply.github.com>
2023-11-23 19:40:57 -0500

    feat: Implement rendering for saffron

--- src/client/java/sernoo/lust/LustClient.java
+++ src/client/java/sernoo/lust/LustClient.java
@@ -1,10 +1,14 @@
 package sernoo.lust;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import sernoo.lust.block.LustBlocks;
 
 public class LustClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		BlockRenderLayerMap.INSTANCE.putBlock(LustBlocks.SAFFRON, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(LustBlocks.POTTED_SAFFRON, RenderLayer.getCutout());
 	}
 }
